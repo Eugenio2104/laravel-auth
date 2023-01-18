8bb167faad29f640701dcea4c8e6bd2a67b83392
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2023-01-18 15:37:13 +0100

    stamp in tabella

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,5 +2,29 @@
 
 
 @section('content')
-    <h1>index</h1>
+    <div class="container">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">name</th>
+                    <th scope="col">slug</th>
+                    <th scope="col">client_name</th>
+                    <th scope="col">summary</th>
+                    <th scope="col">image</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($projects as $project)
+                    <tr>
+                        <th scope="row">{{ $project->id }}</th>
+                        <th scope="row"><img class="thumb" src="{{ $project->cover_image }}" alt=""></th>
+                        <td>{{ $project->name }}</td>
+                        <td>{{ $project->client_name }}</td>
+                        <td>{{ $project->summary }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
